16b2520d16aaca075987f55e8a385d8407e73492
Ali Shahwali <ali_zinq@hotmail.com>
2021-03-05 00:22:01 +0100

    task 3 updated

--- src/Task3/HTTPAsk.java
+++ src/Task3/HTTPAsk.java
@@ -36,8 +36,9 @@ public class HTTPAsk {
 
 
             String request = new String(trimmedData, StandardCharsets.UTF_8);
-            String[] split = request.split("[ ?=&]");
+            String[] split = request.split("[ ?=&\\r\\n]");
 
+            boolean httpVer = false;
             for(int i = 0; i < split.length; i++) {
                 if(split[i].equals("hostname"))
                     host = split[i + 1];
@@ -47,9 +48,11 @@ public class HTTPAsk {
                     str = split[i+1];
                     str += "\n";
                 }
+                else if(split[i].equals("HTTP/1.1"))
+                    httpVer = true;
             }
 
-            if(split[1].equals("/ask") && port != 0) {
+            if(split[0].equals("GET") && split[1].equals("/ask") && port != 0 && httpVer) {
                 try {
                     String output;
                     if(str == null)
